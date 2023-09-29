2ad42b636574e2000a824fe596de87f520a63849
Eunsoly-Lee <dmsthf9596@gmail.com>
2023-09-29 15:04:59 +0900

    Typescript로 수정

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,14 @@
 import { Store } from "../core/coreFile";
 
-export default new Store({
+interface State {
+  photo: string;
+  name: string;
+  email: string;
+  blog: string;
+  github: string;
+  repository: string;
+}
+export default new Store<State>({
   photo: "https://avatars.githubusercontent.com/u/105327508?v=4",
   name: "EUNSOLY / LeeEunSol",
   email: "dmsthf9596@gamil.com",
